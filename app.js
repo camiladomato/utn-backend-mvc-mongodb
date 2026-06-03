8dbe6e271bda63a0865307feeaf60ec12cf70d4f
Camila Domato <camiladomato@gmail.com>
2026-06-02 22:23:49 -0300

    fix app

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 
-const app = express();
+// Importar las rutas
+const authRoutes = require('./routes/auth.routes');
+const taskRoutes = require('./routes/task.routes');
 
+const { errorHandler } = require('./middlewares/error.middleware');
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -10,5 +15,10 @@ app.use(express.json());
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong', status: 'Servidor operativo' });
 });
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+app.use(errorHandler);
 
 module.exports = app;
